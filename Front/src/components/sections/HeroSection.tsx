9f3f107cf23d3cf6597f3917d17d3e8ccc94a84c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-04 23:45:18 -0500

    Update HeroSection.tsx

--- Front/src/components/sections/HeroSection.tsx
+++ Front/src/components/sections/HeroSection.tsx
@@ -1,10 +1,4 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
 import { MainButton } from '@/components/ui/MainButton';
-import { MuteToggle } from '@/components/ui/MuteToggle';
-
-export const HeroSection = () => {
-  const [isMuted, setIsMuted] = useState(true);
-  const videoRef = useRef<HTMLVideoElement>(null);
-
 
