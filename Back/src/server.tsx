6e91f1dfeeb8924a0f7c64e504f66d44ab6fc255
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-05 23:19:43 -0500

    Update server.tsx

--- Back/src/server.tsx
+++ Back/src/server.tsx
@@ -4,9 +4,3 @@ import { connectDB } from './config/database';
 
 const PORT = process.env.PORT || 4000;
 
-// Conectar DB antes de subir el server
-connectDB();
-
-app.listen(PORT, () => {
-  console.log(`💎 Server corriendo en: http://localhost:${PORT}`);
-});
