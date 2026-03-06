4d65d29afb708f53368699c061ff05a7a2ae499e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-05 23:24:22 -0500

    Update app.tsx

--- Back/src/app.tsx
+++ Back/src/app.tsx
@@ -2,14 +2,6 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 
-const app = express();
 
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ message: "Emerald DT API - Security Cluster Active" });
-});
 
 export default app;
